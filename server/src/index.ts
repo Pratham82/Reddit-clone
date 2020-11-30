1454abe262948a762315e8488f1f5921381927a1
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-30 20:37:06 +0530

    added constants and created express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,42 @@
+import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core'
-import { Post } from './entities/Post'
-import { __prod__ } from './constants'
+import { PORT, __prod__ } from './constants'
 import microOrmConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
 
 const main = async () => {
+  // Connecting ORM to postgresql
   const orm = await MikroORM.init(microOrmConfig)
   await orm.getMigrator().up()
   // const post = orm.em.create(Post, { title: 'My first Post' })
   //  await orm.em.persistAndFlush(post)
-  const posts = await orm.em.find(Post, {})
-  console.log(posts)
+  // const posts = await orm.em.find(Post, {})
+  //console.log(posts)
+
+  // Starting express server
+  const app = express()
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({
+      em: orm.em,
+    }),
+  })
+
+  apolloServer.applyMiddleware({
+    app,
+  })
+
+  app.get('/', (_, res) => res.send('Hello from express'))
+
+  app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`))
 }
 
 main()
